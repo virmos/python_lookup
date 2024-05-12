1216de4b361749c9e0e4009bc335c97ba41153d3
virmos <hadonggiang1810@gmail.com>
2024-05-12 20:54:38 +0700

    Add descriptor logging usage

--- descriptor.py
+++ descriptor.py
@@ -160,3 +160,36 @@ vars(h)
 h.x = 55
 vars(h)
 {'_slotvalues': [55, 20]}
+
+
+# ---------------logging---------------
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+class LoggedAccess:
+
+    def __set_name__(self, owner, name):
+        self.public_name = name
+        self.private_name = '_' + name
+
+    def __get__(self, obj, objtype=None):
+        value = getattr(obj, self.private_name)
+        logging.info('Accessing %r giving %r', self.public_name, value)
+        return value
+
+    def __set__(self, obj, value):
+        logging.info('Updating %r to %r', self.public_name, value)
+        setattr(obj, self.private_name, value)
+
+class Person:
+
+    name = LoggedAccess()                # First descriptor instance
+    age = LoggedAccess()                 # Second descriptor instance
+
+    def __init__(self, name, age):
+        self.name = name                 # Calls the first descriptor
+        self.age = age                   # Calls the second descriptor
+
+    def birthday(self):
+        self.age += 1
